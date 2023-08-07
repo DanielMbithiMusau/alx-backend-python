f8586107296a8fb67c818e8cdfed66572870dc5e
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-08-07 11:34:51 +0300

    Measuring runtime

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,6 +4,7 @@
 import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> list:
     ''' Return list of all delays in ascending order '''
     delays = []
