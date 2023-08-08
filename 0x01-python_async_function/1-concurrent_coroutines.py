b26dd8f91f543268a43519ecd1eddbe631cd2bea
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-08-08 18:39:02 +0300

    Fixed error

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,7 +7,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> list:
     ''' Return list of all delays in ascending order '''
-    delays = []
-    for _ in range(n):
-        delays.append(asyncio.create_task(wait_random(max_delay)))
+    delays = [asyncio.create_task(wait_random(max_delay))for _ in range(n)]
     return [await delay for delay in asyncio.as_completed(delays)]
